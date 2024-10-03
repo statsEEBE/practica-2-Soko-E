36c1133783f4b18f60614f8974819274460577d7
soko.christian.echuaka <soko.christian.echuaka@auleseebe.upc.edu>
2024-10-03 11:12:03 +0200

    .

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,9 @@
 #Codigo para problema 1
 
+#datos interpretados de la imagen
+
+# marketvalue = 0.0657*assets+11.79755
+
+# Coefficiente de correlacion = (R^2) = 0.1923
+# coefficiente de bondad de ajuste o determinacion = (adjusted R^2)
+0.0657*647.1+11.79755
--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,19 @@
-#Codigo para problema 2
+mis_dades = iris;
+
+
+Petal.Length = mis_dades$Petal.Length;
+Sepal.Length = mis_dades$Sepal.Length;
+data = data.frame(Petal.Length,Sepal.Length);
+
+# la formula es Sepal.Length = m*Petal.Length+b
+# quitamos los coeffs y tenemos la formula a dar a R
+# la formula ha poner entonces es Sepal.Length ~ Petal.Length
+
+
+linear=lm(Sepal.Length ~ Petal.Length,data)
+#donde los coeficientes son m = petalLength; b = intercept
+linear$coefficients
+
+
+# prediccion si Petal.Length =1.5
+predict.lm(linear,newdata=data.frame(Petal.Length=1.5))
